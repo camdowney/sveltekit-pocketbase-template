12a38acc60e5f829be884c9df7ee9266018c37dc
Cameron Downey <camadowney@gmail.com>
2023-01-08 00:34:53 -0600

    Remove unneeded client hooks

--- src/lib/pocketbase.js
+++ src/lib/pocketbase.js
@@ -4,3 +4,7 @@ import { PUBLIC_POCKETBASE_URL } from '$env/static/public'
 
 export const pb = new PocketBase(PUBLIC_POCKETBASE_URL)
 export const user = writable(pb.authStore.model)
+
+pb.authStore.onChange(() => 
+	user.set(pb.authStore.model)
+)
